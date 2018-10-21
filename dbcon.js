96f92bf7014f66681df4931ee7ab6992d8b369dd
Jared Sulzdorf <j.sulzdorf@gmail.com>
2018-10-21 10:45:41 -0700

    prepping for multiple local dev environments

--- dbcon.js
+++ dbcon.js
@@ -1,11 +1,13 @@
 // This is our DB connection file that we keep abstracted as we'll have to rebuild it once we're done testing locally
 var mysql = require('mysql');
+require('dotenv').config();
+
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'localhost',
-  user            : 'seaowl',
-  password        : 'trojans45',
-  database        : 'test'
+  host            : process.env.HOST,
+  user            : process.env.DB_CON_USER,
+  password        : process.env.DB_CON_PW,
+  database        : process.env.DB_CON_DB
 });
 
 module.exports.pool = pool;
