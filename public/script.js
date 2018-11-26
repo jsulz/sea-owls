717363906b7b8acfba5faf4637853694c54e61d9
Brenna <32725133+brennalyn@users.noreply.github.com>
2018-11-26 00:29:06 -0800

    Event Listeners
    Set event listeners for each edit button on the genres page. When an edit button is clicked, it should autopopulate the new form's values with the genre's id (hidden) and name.

--- public/script.js
+++ public/script.js
@@ -1,1 +1,29 @@
-console.log("hello");
+
+var genres = document.getElementsByClassName("genre-edit");
+
+for each (genre in genres) {
+  genre.addEventListener("click", function(){ displayForm(genre) });
+}
+
+//Displays the hidden edit form and autofills the values of the inputs to the current name/id
+function displayForm(genre) {
+  var id = (genre.id).substr(6);
+  var name = genre.getAttribute("name").;
+  var editform = document.getElementById("genre-edit-form");
+ 
+  document.getElementById("edit-genre-id").value = id;
+  document.getElementById("edit-genre-name").value = name;
+  
+  editform.style.display = "block";
+}
+
+/* The below is code to make the form disappear again when the submit button is hit, 
+but if we are refreshing the page after submit then I guess this won't be necessary?
+
+document.getElementById("edit-genre-submit").addEventListener("click", hideForm(genre));
+
+function hideForm() {
+  var editform = document.getElementById("genre-edit-form");
+  editform.style.display = "none";
+}
+*/
